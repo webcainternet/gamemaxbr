429eae52f8f7a043e418e88ded9c488399ae5996
fernandofmendes <fernando.mendes@webca.com.br>
2015-07-03 18:53:17 -0300

    font bold

--- public/wp-content/themes/theme53386/wrapper/wrapper-header.php
+++ public/wp-content/themes/theme53386/wrapper/wrapper-header.php
@@ -13,10 +13,10 @@
 		<?php get_template_part("static/static-logo"); ?>
 	</div>
 
-	<div style="float: right; width: 400px; margin-top: 35px;">
+	<div style="float: right; margin-top: 35px;">
 		<input type="text" placeholder="O que você procura?" style="width: 250px; height: 40px; border: solid 1px;">
 		<input type="submit" value="Buscar" style="background-color: #7AA032; color: #000000; padding: 10px;height: 40px;
-  margin-top: -10px;  font-size: 12px;">
+  margin-top: -10px;  font-size: 12px; font-weight: bold;">
 	</div>
 
 
