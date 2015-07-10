19acfab70b27aa2f1ab9ca7e64a8f7d39a0afeee
fernandofmendes <fernando.mendes@webca.com.br>
2015-07-10 18:52:10 -0300

    mapa

--- public/wp-content/themes/theme53386/page-onde-comprar.php
+++ public/wp-content/themes/theme53386/page-onde-comprar.php
@@ -9,21 +9,24 @@ get_header(); ?>
 	<div class="container">
 		<div class="row">
 
-		<h1>11111
 			<div class="<?php echo cherry_get_layout_class( 'full_width_content' ); ?>" data-motopress-wrapper-file="page-fullwidth.php" data-motopress-wrapper-type="content">
 				<div class="row">
 					<div class="<?php echo cherry_get_layout_class( 'full_width_content' ); ?>" data-motopress-type="static" data-motopress-static-file="static/static-title.php">
 						<?php get_template_part("static/static-title"); ?>
 					</div>
 				</div>
+
 				<div id="content" class="row">
+
+							<hr>11111
+
 					<div class="<?php echo cherry_get_layout_class( 'full_width_content' ); ?>" data-motopress-type="loop" data-motopress-loop-file="loop/loop-page.php">
 						<?php get_template_part("loop/loop-page"); ?>
 					</div>
+
+							<hr>11111
 				</div>
 			</div>
-
-<h1>11111
 		</div>
 	</div>
 </div>
