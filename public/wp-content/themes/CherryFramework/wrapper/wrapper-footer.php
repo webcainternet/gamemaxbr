5f75d28fbd71fa4d8e6c97f4239afb7926f14961
fernandofmendes <fernando.mendes@webca.com.br>
2015-07-10 11:49:42 -0300

    a

--- public/wp-content/themes/CherryFramework/wrapper/wrapper-footer.php
+++ public/wp-content/themes/CherryFramework/wrapper/wrapper-footer.php
@@ -6,13 +6,17 @@
 	<div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-2">
 		<?php dynamic_sidebar("footer-sidebar-2"); ?>
 	</div>
-	<div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-3">
-		<?php dynamic_sidebar("footer-sidebar-3"); ?>
-	</div>
+	
 	<div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-4">
 		<?php dynamic_sidebar("footer-sidebar-4"); ?>
 	</div>
+
+	<div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-3">
+		<?php dynamic_sidebar("footer-sidebar-3"); ?>
+	</div>
+	
 </div>
+
 <div class="row copyright">
 	<div class="span6" data-motopress-type="static" data-motopress-static-file="static/static-footer-text.php">
 		<?php get_template_part("static/static-footer-text"); ?>
