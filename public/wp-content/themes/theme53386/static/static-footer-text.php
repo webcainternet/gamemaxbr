9645888fa134c6f010bb6c133255095ca495f7fe
fernandofmendes <fernando.mendes@webca.com.br>
2015-07-18 18:54:38 -0300

    footer

--- public/wp-content/themes/theme53386/static/static-footer-text.php
+++ public/wp-content/themes/theme53386/static/static-footer-text.php
@@ -4,7 +4,7 @@
 	<?php if($myfooter_text){?>
 		<?php echo of_get_option('footer_text'); ?>
 	<?php } else { ?>
-		<a href="<?php echo home_url(); ?>/" title="<?php bloginfo('description'); ?>" class="site-name"><?php bloginfo('name'); ?></a> <span>&copy; <?php echo date('Y'); ?> <em>|</em> <a href="<?php echo home_url(); ?>/privacy-policy/" title="<?php echo theme_locals('privacy_policy'); ?>"><?php echo theme_locals("privacy_policy"); ?></a></span>
+		<?php echo date('Y'); ?> © Gamemax - Todos os direitor reservados
 	<?php } ?>
 	<?php if( is_front_page() ) { ?>
 		<?php echo date('Y'); ?> © Gamemax - Todos os direitor reservados
