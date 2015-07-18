8569f98ad57362490e2601060e762815760e8c37
fernandofmendes <fernando.mendes@webca.com.br>
2015-07-18 18:58:09 -0300

    navigation

--- public/wp-content/themes/theme53386/static/static-footer-nav.php
+++ public/wp-content/themes/theme53386/static/static-footer-nav.php
@@ -1,7 +1,7 @@
 <?php /* Static Name: Footer navigation */
 	if (has_nav_menu('footer_menu')) {
 		if ( of_get_option('footer_menu') == 'true') { ?>  
-		<div><h4><?php echo __('Navigation', CURRENT_THEME) ?></h4></div>
+		<div><h4><?php echo __('Navegação', CURRENT_THEME) ?></h4></div>
 		<nav class="nav footer-nav">
 			<?php wp_nav_menu( array(
 				'container'       => 'ul',
