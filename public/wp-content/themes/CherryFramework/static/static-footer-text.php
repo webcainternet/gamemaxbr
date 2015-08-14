29554a9f8054e8b3f1354579013852bcc9cc1bdf
fernandofmendes <fernando.mendes@webca.com.br>
2015-08-14 19:12:49 -0300

    footer

--- public/wp-content/themes/CherryFramework/static/static-footer-text.php
+++ public/wp-content/themes/CherryFramework/static/static-footer-text.php
@@ -3,7 +3,7 @@
 	<?php $myfooter_text = apply_filters( 'cherry_text_translate', of_get_option('footer_text'), 'footer_text' ); ?>
 
 	<?php if($myfooter_text){?>
-		<?php echo date('Y'); ?> © Gamemax - Todos os direitor reservados
+		<?php echo $myfooter_text; ?>
 	<?php } else { ?>
 		<?php echo date('Y'); ?> © Gamemax - Todos os direitor reservados
 	<?php } ?>
