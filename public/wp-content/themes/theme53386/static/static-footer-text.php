e226db9e3535ffdd4285aab6c94f3466819f3783
fernandofmendes <fernando.mendes@webca.com.br>
2015-08-14 19:15:18 -0300

    Revert "footer"
    This reverts commit 29554a9f8054e8b3f1354579013852bcc9cc1bdf.

--- public/wp-content/themes/theme53386/static/static-footer-text.php
+++ public/wp-content/themes/theme53386/static/static-footer-text.php
@@ -2,7 +2,7 @@
 <div id="footer-text" class="footer-text">
 	<?php $myfooter_text = of_get_option('footer_text'); ?>
 	<?php if($myfooter_text){?>
-		<?php echo of_get_option('footer_text'); ?>
+		<?php echo date('Y'); ?> © Gamemax - Todos os direitor reservados
 	<?php } else { ?>
 		<?php echo date('Y'); ?> © Gamemax - Todos os direitor reservados
 	<?php } ?>
